4afb47fdcc703bb66df44fe6f5868ffe1072ee07
Girdhari002 <girdharisinghyadav4343@gmail.com>
2026-08-31 23:55:06 +0530

    fix gmail smtp connection

--- server/services/mailer.js
+++ server/services/mailer.js
@@ -10,13 +10,16 @@ const getTransporter = () => {
     return null;
   }
 
-  transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+ transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  family: 4,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
   return transporter;
 };
 
